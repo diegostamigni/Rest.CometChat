789809df4310e91488fad05fcb9052163ed8f8bf
Diego Stamigni <diego.stamigni@gmail.com>
2021-06-08 15:14:33 +0100

    `ICometChatConfig` to have nullable props

--- Rest.CometChat.Abstractions/ICometChatConfig.cs
+++ Rest.CometChat.Abstractions/ICometChatConfig.cs
@@ -2,13 +2,13 @@ namespace Rest.CometChat.Abstractions
 {
 	public interface ICometChatConfig
 	{
-		public string AppId { get; set; }
+		public string? AppId { get; set; }
 
-		public string ApiKey { get; set; }
+		public string? ApiKey { get; set; }
 
 		/// <summary>
 		/// The region on which your CometChat application resides. Typically either `us` or `eu`.
 		/// </summary>
-		public string Region { get; set; }
+		public string? Region { get; set; }
 	}
 }
